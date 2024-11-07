77e1922ab49b8fe00c1ce7cd72c456d970faaf4a
Christián Andres Tesén Becerra <74812707+christiantesen@users.noreply.github.com>
2024-11-07 01:18:39 +0000

    .

--- frontend/src/views/Index copy.vue
+++ frontend/src/views/Index copy.vue
@@ -28,14 +28,14 @@
                 target="_blank"
                 class="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-emerald-500 active:bg-emerald-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
               >
-                REGISTRARSE
+                Get started
               </a>
               <a
                 href="https://github.com/creativetimofficial/vue-notus?ref=vn-index"
                 class="github-star ml-1 text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-700 active:bg-blueGray-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
                 target="_blank"
               >
-                LANDING
+                Github Star
               </a>
             </div>
           </div>
